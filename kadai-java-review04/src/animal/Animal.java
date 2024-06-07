a2d7a118bf16b5a125463567a69ad9ea94e2d7a5
araimonotanomu <sevenscoad7@gmail.com>
2024-06-07 21:07:52 +0900

    再提出

--- kadai-java-review04/src/animal/Animal.java
+++ kadai-java-review04/src/animal/Animal.java
@@ -5,20 +5,8 @@ public class Animal {
     private String name;
     private int age;
     
-    
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
+    public Animal(String name, int age) {
         this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
         this.age = age;
     }
 
